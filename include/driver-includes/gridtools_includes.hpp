7e278f8a030097a99502e57784c4ce56528dc7fc
Eddie Davis <eddied@vulcan.com>
2020-05-30 08:59:55 -0700

    Update gridtools_includes so 'gtmock' can be swapped for 'gridtools'

--- include/driver-includes/gridtools_includes.hpp
+++ include/driver-includes/gridtools_includes.hpp
@@ -5,4 +5,7 @@
 #ifndef DAWN_GT4PY_TEST_GRIDTOOLS_INCLUDES_HPP
 #define DAWN_GT4PY_TEST_GRIDTOOLS_INCLUDES_HPP
 
+#include "../gridtools/common/defs.hpp"
+#include "../gridtools/common/gt_math.hpp"
+
 #endif // DAWN_GT4PY_TEST_GRIDTOOLS_INCLUDES_HPP
